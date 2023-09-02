fcf08fa2e798f38dbf3895901704169578879186
Noman Mumtaz <115913726+nomaannm@users.noreply.github.com>
2023-09-02 12:24:52 +0530

    Time Limit exceeded

---
+++ DividieWithoutDivide.java
@@ -0,0 +1,29 @@
+/**
+@author Nomaan Mumtaz || nomanm2324@gmail.com
+*/
+/* Time Limit Exceeded
+public class DivideWithoutDivide{
+    public static int divide(int dividend , int divisor)
+    {
+        int rem = divisor - 1;
+        int quotient = 0;
+        while(divisor > rem)
+        {
+            dividend -= divisor;
+            quotient ++;
+        }
+
+        return quotient ;
+
+        
+
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(divide(8,2));
+    }
+}
+*/
+
+
