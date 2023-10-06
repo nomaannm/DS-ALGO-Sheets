435407e42637050c62047c357c61bbd9d0a130bd
Noman Mumtaz <115913726+nomaannm@users.noreply.github.com>
2023-10-07 01:31:10 +0530

    Create PowerOfThree.cpp

---
+++ PowerOfThree.cpp
@@ -0,0 +1,14 @@
+/**
+* Question No. 326
+* TopSWE.com
+
+*/
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n == 0) return false ;
+        else if(n == 1) return true ;
+        return n%3 == 0 && isPowerOfThree(n/3);
+        
+    }
+};
