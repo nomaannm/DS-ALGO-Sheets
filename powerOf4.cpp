16d60642637e7045c87d80d03b673810c2eecb80
Noman Mumtaz <nomanm2324@gmail.com>
2023-11-13 11:40:47 +0530

    Create powerOf4.cpp

---
+++ powerOf4.cpp
@@ -0,0 +1,12 @@
+// using logarithmic
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n <= 0) return false ;
+        int a = log2(n)/log2(4) ;
+        if(pow(4,a) == n) return true ;
+        else return false ;
+        
+        
+    }
+};
