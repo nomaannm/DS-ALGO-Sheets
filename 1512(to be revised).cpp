2a04c47fb27731591e794984352ec1686df7fd4e
Noman Mumtaz <nomanm2324@gmail.com>
2023-11-19 01:45:05 +0530

    Create 1512(to be revised).cpp

---
+++ 1512(to be revised).cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int number = 0 ;
+        for(int i{0} ; i != nums.size() ; i++){
+            for(int j{i+1} ; j != nums.size() ; j++){
+                if(nums[i] == nums[j] && i < j) number ++;
+
+            }
+        }
+        return number ;
+
+        
+    }
+};
