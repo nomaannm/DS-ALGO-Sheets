2f530eedfd89cea5482b7303bfd6dc42c8d643b3
Noman Mumtaz <nomanm2324@gmail.com>
2024-06-19 16:11:16 +0530

    Create LowerBoundAlgorithm.cpp

---
+++ Algorithms/LowerBoundAlgorithm.cpp
@@ -0,0 +1,24 @@
+// Implemented using BINARY SEARCH
+int bs(vector<int>& arr, int n , int target){
+	int low = 0;
+	int high = n-1;
+	int ans = n;
+
+	while(low <= high){
+		int mid = (low + high)/2;
+		if(arr[mid] >= target){
+			ans = mid;
+			high = mid-1;
+		}
+		else{
+			low = mid + 1;
+		}
+	}
+
+	return ans;
+}
+
+int lowerBound(vector<int> arr, int n, int x) {
+	return bs(arr,n,x);
+	
+}
