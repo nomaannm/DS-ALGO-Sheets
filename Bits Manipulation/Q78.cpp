c24b507ad8ccb573516ec36014a936f205284cbf
Noman Mumtaz <nomanm2324@gmail.com>
2024-06-24 17:04:02 +0530

    Create Q78.cpp

---
+++ Bits Manipulation/Q78.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+
+        for(int i = 0; i < (1 << n); i++ ){
+            vector<int> set;
+            for(int j = 0; j < n; j++){
+                if((i & (1 << j)) > 0) {
+                    set.push_back(nums[j]);
+                } 
+            }
+            ans.push_back(set);
+        }
+
+        return ans;
+    }
+};
