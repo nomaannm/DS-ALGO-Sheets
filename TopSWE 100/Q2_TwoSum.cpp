e1f9cf0f7b342548c8a6319be33486b7d4619d18
Noman Mumtaz <nomanm2324@gmail.com>
2024-06-28 19:05:57 +0530

    Create Q2_TwoSum.cpp

---
+++ TopSWE 100/Q2_TwoSum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int> mpp;
+        for(int i = 0 ; i < nums.size() ; i++){
+            int a = nums[i];
+            int more = target - a;
+            if(mpp.find(more) != mpp.end()){
+                return {i , mpp[more]};
+
+            }
+            mpp[a] = i;
+        }
+        return {-1,-1};
+    }
+    
+};
