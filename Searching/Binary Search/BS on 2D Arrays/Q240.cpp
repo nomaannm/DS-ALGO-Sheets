c36bdbbb599dbf2daaf7d3ba600155d8007992f0
Noman Mumtaz <nomanm2324@gmail.com>
2024-06-30 12:29:58 +0530

    Create Q240.cpp

---
+++ Searching/Binary Search/BS on 2D Arrays/Q240.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = 0, col = matrix[0].size()-1;
+        while(row < matrix.size() && col >= 0){
+            if(matrix[row][col] == target) return true;
+            else if(matrix[row][col] < target) row += 1;
+            else col--;
+        }
+
+        return false;
+        
+    }
+};
