10d48039ea16f060005a82e4133c3202ed71d321
Noman Mumtaz <nomanm2324@gmail.com>
2024-07-21 16:16:26 +0530

    Create Q160.cxx

---
+++ Linked Lists/Singly LL/Q160.cxx
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if(headA == NULL || headB  == NULL) return NULL;
+        ListNode* t1 = headA;
+        ListNode* t2 = headB;
+        while( t1 != t2){
+            t1 = t1->next;
+            t2 = t2->next;
+            if(t1 == t2) break;
+            if(t1 == NULL) t1 = headB;
+            if(t2 == NULL) t2 = headA;
+        }
+
+        return t1;
+
+        
+    }
+};
