952117de80777f663005b12612d6cddaefafb4f7
Noman Mumtaz <nomanm2324@gmail.com>
2024-09-21 23:38:34 +0530

    Update Q_TheCelebrityProblem_GFG.cxx

--- Stacks/Q_TheCelebrityProblem_GFG.cxx
+++ Stacks/Q_TheCelebrityProblem_GFG.cxx
@@ -2,25 +2,30 @@ class Solution {
   public:
     // Function to find if there is a celebrity in the party or not.
     int celebrity(vector<vector<int> >& mat) {
-        vector<int> knowMe(mat.size(),0);
-        vector<int> iKnowThem(mat.size(),0);
+        int top = 0;
+        int bottom = mat.size()-1;
+        while(top < bottom){
+            if(mat[top][bottom] == 1){
+                top++;
+            }
+            else if(mat[bottom][top] == 1){
+                bottom--;
+            }
+            else{
+                top++;
+                bottom--;
+            }
+        }
+        if(top > bottom) return -1;
         
         for(int i=0; i < mat.size(); i++){
-            for(int j=0; j < mat[0].size(); j++){
-                if(mat[i][j] == 1){
-                    knowMe[j]++;
-                    iKnowThem[i]++;
-                }
-            }
+            if(i == top) continue;
+            if(mat[top][i] == 0 && mat[i][top] == 1) return top;
+            else return -1;
         }
         
-        for(int i =0; i < mat.size(); i++){
-            if(knowMe[i] == mat.size()-1 && iKnowThem[i] == 0 ){
-                return i;
-            }
-        }
+        return top;
         
-        return -1;
         
     }
 };
