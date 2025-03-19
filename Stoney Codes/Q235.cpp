70badb9e70ca7f64622af635e851b058f476ac71
Noman Mumtaz <nomanm2324@gmail.com>
2025-03-19 16:03:43 +0530

    Create Q235.cpp

---
+++ Stoney Codes/Q235.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while(root){
+            if(min(p->val, q->val) > root->val){
+                root = root->right;
+            } else if(max(p->val, q->val) < root->val){
+                root = root->left;
+            } else {
+                return root;
+            }
+        }
+        return nullptr;
+    }
+};
