fca63e8601db48ad761e3dd9bdde5152e558b5e7
Noman Mumtaz <nomanm2324@gmail.com>
2025-04-13 16:58:43 +0530

    Update FermatsTheorem.cpp

--- Number Theory/FermatsTheorem.cpp
+++ Number Theory/FermatsTheorem.cpp
@@ -29,17 +29,54 @@ long long modularExponentiationPolicy(int base, int expo, int mod) {
 
 }
 
-void modularInverse(int a, int m) {
+long long modularInverse(long long a, long long m) {
     // GCD(a,m) != 1, Inverse doesn't exists
     if (GCD(a, m) != 1) {
-        cout << "Modular inverse does not exist" << endl;
+        return -1;
     }
     // Modular Inverse of a % m = modular exponentiation (a, m-2)%m;
     int modularInverse = modularExponentiationPolicy(a, (m-2), m);
-    cout << modularInverse << endl;
+    return modularInverse;
 
 }
 
+//We have to compute nCr % p
+// nCr = n!/r!*(n-r)!
+
+int nChoosingR(int n, int r) {
+    if (n == 0 ) return 0;
+    if (r == 0 || r == n) return 1;
+    if (r == 1) return n;
+
+    return nChoosingR(n-1, r-1) + nChoosingR(n-1, r);
+}
+
+
+
+// But using fermat little
+int fermatLittleChoosingPolicy(int n, int r, int m) {
+    if (r < 0 || r > n || n==0) return 0;
+    if (r== 0 || r == n) return 1;
+    if (r == 1) return n;
+
+    long long numerator = 1LL;
+    long long denominator = 1LL;
+    for (int i=0; i < r; i++) {
+        numerator = (numerator * (n - i)) % m;
+        denominator = (denominator * (i + 1)) % m;
+    }
+
+    return int(numerator * modularInverse(denominator, m)) % m;
+
+
+}
+
+
+
+
+
+
 int main() {
-    modularInverse(3, 7);
+    cout << nChoosingR(10, 2) << endl;
+    cout << fermatLittleChoosingPolicy(10, 2, 1000000007) << endl;
 }
