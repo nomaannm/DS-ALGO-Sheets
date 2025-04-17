a126fe9ea3e7c69c0ae81f2a115db6b44fba74a6
Noman Mumtaz <nomanm2324@gmail.com>
2025-04-17 13:33:13 +0530

    Create EulerTotient.cpp

---
+++ Number Theory/EulerTotient.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+int eulerTotient(int n) {
+    int result = n;
+    for (int i = 2; i*i <= n; i++) {
+        if (n % i == 0) {
+            while (n % i == 0) {
+                n /= i;
+            }
+            result -= result / i;
+        }
+    }
+    if (n > 1) {
+        result -= result / n;
+    }
+    return result;
+}
+
+
+int main() {
+    cout << eulerTotient(8) << endl;
+
+}
